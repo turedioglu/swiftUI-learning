fa1674b53a9a32d18df6ac89ac27f8fbd97ce964
turedioglu <fahrettin098@hotmail.com>
2022-06-01 16:33:38 +0300

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Turedioglu on 1.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
